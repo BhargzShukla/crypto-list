e821dea4173b350dec5824102731c456da230e55
Bhargav Shukla <shukla.bhargav.k@gmail.com>
2020-12-30 16:21:16 +0530

    Add site footer with data provider attribution, project author details

---
+++ src/components/SiteFooter.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer
+    class="flex flex-col items-center justify-center w-screen px-4 py-6 bg-gray-200 shadow-inner"
+  >
+    <p>
+      Created by
+      <a
+        href="https://bhargavshukla.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="text-blue-500 hover:underline"
+        >Bhargav Shukla</a
+      >.
+    </p>
+    <small>Data provided by Coinranking.</small>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'SiteFooter'
+}
+</script>
+
+<style></style>
