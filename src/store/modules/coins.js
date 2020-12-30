89d6141b98967e14b93616c7c318e8236f846e13
Bhargav Shukla <shukla.bhargav.k@gmail.com>
2020-12-30 16:17:49 +0530

    Add API to Vuex store, update getters and actions for new state

--- src/store/modules/coins.js
+++ src/store/modules/coins.js
@@ -1,26 +1,61 @@
-import coins from '../../api/coins'
+import axios from 'axios'
 
 // initial state
 const state = () => ({
-  all: []
+  coins: [],
+  limit: 10,
+  offset: 0
 })
 
 // getters
-const getters = {}
+const getters = {
+  coins: (state) => {
+    return state.coins
+  },
+  limit: (state) => {
+    return state.limit
+  },
+  offset: (state) => {
+    return state.offset
+  }
+}
 
 // actions
 const actions = {
-  getAllCoins({ commit }) {
-    coins.getTopCoins((coins) => {
-      commit('setCoins', coins)
-    })
+  getAllCoins({ state, commit }) {
+    axios
+      .get('/api/coins', {
+        params: {
+          limit: state.limit,
+          offset: state.offset
+        }
+      })
+      .then((response) => {
+        const coins = response.data.data.coins
+        commit('setCoins', coins)
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+  },
+  updateLimit({ commit }, limit) {
+    commit('setLimit', limit)
+  },
+  updateOffset({ commit }, offset) {
+    commit('setOffset', offset)
   }
 }
 
 // mutations
 const mutations = {
   setCoins(state, coins) {
-    state.all = coins
+    state.coins = coins
+  },
+  setLimit(state, limit) {
+    state.limit = limit
+  },
+  setOffset(state, offset) {
+    state.offset = offset
   }
 }
 
